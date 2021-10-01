cfb96033b0dee2739fa4e9346c86bf405bb30914
jingxian <jingxianchai01@gmail.com>
2021-10-01 10:50:02 -0500

    week 4 lab small changes

--- labs/week4-word-count/word_count.py
+++ labs/week4-word-count/word_count.py
@@ -68,19 +68,20 @@ def count(words):
 
 # calculate the normalization of given counts dictionary
 # return a normalized dictionary
-def normalize(word_dct):
+def normalize(counts):
     square_sum = 0
-    for count in word_dct.values():
+    for count in counts.values():
         square = count ** 2
         square_sum += square
     size = math.sqrt(square_sum)
     nrm_dct = {}  # normalized dictionary
-    for word, count in word_dct.items():
-        nrm_dct[word] = 0 if size == 0 else count / size  # avoiding divide by zero
+    for word, count in counts.items():
+        nrm_dct[word] = 0 if size == 0 else count / \
+            size  # avoiding divide by zero
     return nrm_dct
 
 
-# comparing two normalized dictionary, if word(key) in nrm_dct1 does not exists in nrm_dct2,
+# comparing two normalized dictionary, if word (key) in nrm_dct1 does not exists in nrm_dct2,
 # then add the word as key, 0 as the value into nrm_dct2
 # and do the same thing to nrm_dct1
 def compare(nrm_dct1, nrm_dct2):
@@ -106,9 +107,11 @@ def word_count_distance(count1, count2):
 def most_common_word(counts):
     max_count = 0
     mcw_set = set()  # most common word set
+    # find maximum value of count
     for word, count in counts.items():
         if count > max_count and word not in too_common_words:
             max_count = count
+    # find word with maximum value of count, and insert into th set
     for word, count in counts.items():
         if count == max_count and word not in too_common_words:
             mcw_set.add(word)
@@ -116,14 +119,18 @@ def most_common_word(counts):
 
 
 def most_similar(samples):
+    # dictionary (key: distance score, value: label pair)
     distance_score_with_pair = {}
-    for first, first_counts in samples.items():
-        for second, seocnd_counts in samples.items():
-            if first != second:
-                distance_score = word_count_distance(first_counts, seocnd_counts)
-                distance_score_with_pair[distance_score] = (first, second)
-    if distance_score_with_pair:
+    for first_label, first_counts in samples.items():
+        for second_label, seocnd_counts in samples.items():
+            # prevent comparing itself, and skip if pair exist in dictionary
+            if first_label != second_label and (first_label, second_label) not in distance_score_with_pair.values():
+                distance_score = word_count_distance(
+                    first_counts, seocnd_counts)
+                distance_score_with_pair[distance_score] = (
+                    first_label, second_label)
+    if distance_score_with_pair:  # check if any pair is in the dictionary
         min_distance_score = min(distance_score_with_pair.keys())
         return distance_score_with_pair[min_distance_score]
-    else:
+    else:  # return empty set if no pair is in the dictionary
         return ()
