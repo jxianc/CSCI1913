fd9fec20dd90ee166d549b79e34a070340a9560e
jingxian <jingxianchai01@gmail.com>
2021-10-19 05:27:27 -0500

    remove algorithm file

--- searching_algorithm/linear_search.py
+++
@@ -1,5 +0,0 @@
-def linear_search(array, target):
-    for i in range(len(array)):
-        if array[i] == target:
-            return i
-    return "Not Found"
--- searching_algorithm/test.py
+++
@@ -1,6 +0,0 @@
-from linear_search import *
-from binary_search import *
-
-arr = [1, 3, 4, 7, 9, 13, 24, 28, 37, 69]
-index = binary_search(arr, 13)
-print(index)
